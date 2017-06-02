9d2627cd71824841351c6924ed7e8b01fb8d3e49
PedroFerreira83 <pedro.rosendo@gmail.com>
2017-06-02 11:03:44 +0100

    format text

--- Question1/src/pf/techtest/question1/TechTest1.java
+++ Question1/src/pf/techtest/question1/TechTest1.java
@@ -1,6 +1,7 @@
-
 package pf.techtest.question1;
+
 import java.util.Arrays;
+
 /**
  *
  * @author Pedro Ferreira
@@ -17,13 +18,14 @@ public class TechTest1 {
         System.out.println(checkAnagrams("Snooze alarms", "Alas! No more Zs"));
         System.out.println(checkAnagrams("Halley's Comet", "Shall yet come"));
         System.out.println(checkAnagrams("One good turn deserves another.", "Do rogues endorse that? No,never!"));
-       
+
     }
 
     /**
      * Given two strings, check if they’re anagrams or not
      * <p>
      * This method checks if two strings are anagrams and return the result
+     *
      * @param string1 the first string to check
      * @param string2 the second string to check
      * @return the result of anagram validation
@@ -31,9 +33,9 @@ public class TechTest1 {
     public static boolean checkAnagrams(String string1, String string2) {
 
         //remove non char and use lower case
-        String testString1= string1.replaceAll("[^a-zA-Z]","").toLowerCase();
-       String testString2= string2.replaceAll("[^a-zA-Z]","").toLowerCase();         
-         
+        String testString1 = string1.replaceAll("[^a-zA-Z]", "").toLowerCase();
+        String testString2 = string2.replaceAll("[^a-zA-Z]", "").toLowerCase();
+
         // string to array
         char[] testArray1 = testString1.toCharArray();
         char[] testArray2 = testString2.toCharArray();
@@ -41,7 +43,7 @@ public class TechTest1 {
         // sort arrays
         Arrays.sort(testArray1);
         Arrays.sort(testArray2);
-        
+
         //check if arrays are equals
         return Arrays.equals(testArray1, testArray2);
     }
